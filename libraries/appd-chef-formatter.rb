29c59a620f89871d219837940028e24bf8512af0
Etienne Garnier <garnier.etienne@gmail.com>
2013-10-06 17:42:45 +0200

    update the chef formatter

--- libraries/appd-chef-formatter.rb
+++ libraries/appd-chef-formatter.rb
@@ -7,24 +7,24 @@ class Chef
       def resource_action_start(resource, action, notification_type = nil, notifier = nil)
         if resource.cookbook_name && resource.recipe_name
           resource_recipe = "#{resource.cookbook_name.downcase} #{resource.recipe_name.downcase if resource.recipe_name.downcase != "default"}:"
-        else
-          resource_recipe = "<Dynamically Defined Resource>"
-        end
+      
+          if resource_recipe != @current_recipe
+            puts "Configuring #{resource_recipe}"
+            @current_recipe = resource_recipe
+          end
 
-        if resource_recipe != @current_recipe
-          puts "Configuring #{resource_recipe}"
-          @current_recipe = resource_recipe
+          print "* #{action} '#{resource.name}' #{resource.resource_name if resource.resource_name != :execute}..." if action != :nothing
         end
-
-        print "* #{action} '#{resource.name}' #{resource.resource_name if resource.resource_name != :execute}..." if action != :nothing
       end
 
       def resource_up_to_date(resource, action)
-        puts "\b\b\b\b (nothing to do)"
+        # puts "\b\b\b\b (nothing to do)"
+        puts " (nothing to do)"
       end
 
       def resource_updated(resource, action)
-        puts "\b\b\b\b (done)"
+        # puts "\b\b\b\b (done)"
+        puts " (done)"
       end
     end
   end
