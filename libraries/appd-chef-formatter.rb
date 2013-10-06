306fef4017e343c81583a136fc2c5aa9f45801f1
Etienne Garnier <garnier.etienne@gmail.com>
2013-10-06 18:10:44 +0200

    update the chef formatter

--- libraries/appd-chef-formatter.rb
+++ libraries/appd-chef-formatter.rb
@@ -27,7 +27,8 @@ class Chef
       end
 
       def resource_skipped(resource, action, conditional)
-        puts "\b\b\b\b (#{resource.cookbook_name}: nothing to do)" if print_resource?(resource)
+        # puts "\b\b\b\b (#{resource.cookbook_name}: nothing to do)" if print_resource?(resource)
+        puts " (skipped due to #{conditional.short_description})" if print_resource?(resource)
       end
 
       private
